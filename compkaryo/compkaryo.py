68a95f792aa292fc2f91eaccfac658e8f64aa13c
Becca Love <rlove1@nd.edu>
2018-11-23 13:05:12 -0500

    before sample_bool and removing bi_bool

--- compkaryo/compkaryo.py
+++ compkaryo/compkaryo.py
@@ -11,13 +11,6 @@ import argparse
 import numpy as np
 import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument("vcf", help="path to variant call format file \
-                    containing the genotypes")
-parser.add_argument("inversion", help="inversion to be classified",
-                    choices=["2La","2Rj","2Rb","2Rc","2Rd","2Ru"])
-parser.add_argument("-o", "--out", help="name of the results file")
-args = parser.parse_args()
 
 ##read in the predictive SNPs for the inversion of interest
 
@@ -25,21 +18,35 @@ args = parser.parse_args()
 
 ##calculate the average genotype across the predictive SNPs
 
-##return the average genotype and the number of sites across which it was calculated
+##return the average genotype and the # of sites across which it was calculated
 
 inversionDict = {"2La" : ("2L","path/to/2La"),
                  "2Rj" : ("2R","path/to/2Rj"),
                  "2Rb" : ("2R","path/to/2Rb"),
                  "2Rc" : ("2R","path/to/2Rc"),
                  "2Rd" : ("2Rd","path/to/2Rd"),
-                 "2Ru" : ("2Ru","path/to/2Ru")}
+                 "2Ru" : ("2Ru","path/to/2Ru"),
+                 "test" : ("fooY","../test/test_targets_clean.txt")}
+
+def parse_args():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("vcf", help="path to variant call format file \
+                containing the genotypes")
+    parser.add_argument("inversion", help="inversion to be classified",
+                choices=["2La","2Rj","2Rb","2Rc","2Rd","2Ru","test"])
+    parser.add_argument("-o", "--out", help="name of the results file")
+    
+    args = parser.parse_args()
+
+    return args
 
 
 def import_data(callset_path):
     
     callset = allel.read_vcf(callset_path,
-    fields = ['samples','calldata/GT','calldata/GQ','variants/CHROM',
-              'variants/FILTER_PASS', 'variants/POS','variants/QUAL',
+    fields = ['samples','calldata/GT','variants/CHROM',
+              'variants/FILTER', 'variants/POS',
               'variants/REF','variants/ALT'])
     
     return callset
@@ -52,9 +59,10 @@ def import_inversion(inversion):
     
     return concordant_pos_list
 
-def extract_vtbl_indices(concordant_pos_list, callset):
+def extract_vtbl_indices(concordant_pos_list, callset, chrom=None):
     
-    chrom = inversionDict[args.inversion][0]
+    if not chrom:
+        chrom = inversionDict[args.inversion][0]
     
     indices = []
 
@@ -77,7 +85,7 @@ def extract_biallelic_SNPs(callset, indices):
     
     bi_bool =\
     allel.GenotypeArray(callset["calldata/GT"][indices]).count_alleles().\
-    is_biallelic()
+    max_allele() <= 1
     
     return bi_bool
     
@@ -125,7 +133,11 @@ def main():
         
         for i in range(len(av_gts)):
         
-            out_record = (av_gts[i],"\t",total_sites[i],"\n")
+            record = (str(av_gts[i]),str(total_sites[i]))
+            out_record = "\t".join(record) + "\n"
+            
+            '''pickedRecord = random.choice(records)
+            alignRecord = "\t".join(pickedRecord) + "\n"'''
                 
             out.write(out_record)
             
@@ -135,6 +147,7 @@ def main():
             
             out.close()
     
-if __name__ == "__main":
+if __name__ == "__main__":
+    args = parse_args()
     main()
 
